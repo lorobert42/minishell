e834aafb1ed5ecb47e4a647bc9dbcd1f6b1f738f
lorobert <lorobert@student.42lausanne.ch>
2023-02-10 10:34:06 +0100

    echo base

---
+++ sources/builtins/ft_echo.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lorobert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 09:40:38 by lorobert          #+#    #+#             */
+/*   Updated: 2023/02/10 10:26:23 by lorobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdbool.h"
+#include "stdio.h"
+#include "libft.h"
+
+static bool	is_n(char *arg)
+{
+	int	i;
+
+	if (arg[i] != '-')
+		return (false);
+	i++;
+	while (arg[i] == 'n')
+		i++;
+	if (arg[i] != '\0')
+		return (false);
+	return (true);
+}
+
+int	ft_echo(char **args, int fd)
+{
+	int		i;
+	bool	n;
+
+	i = 0;
+	while (args[i] && is_n(args[i]))
+	{
+		n = true;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], fd);
+		if (args[i + 1] && args[i + 1][0] != '\0')
+			ft_putchar_fd(' ', fd);
+		i++;
+	}
+	if (!n)
+		ft_putchar_fd('\n', fd);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	return (ft_echo(argv[1], 1));
+}
