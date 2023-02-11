e5debcbbb4290606dd881445f2633ec08952ce08
lorobert <lorobert@student.42lausanne.ch>
2023-02-11 18:26:18 +0100

    Use getcwd since it's allowed

--- sources/builtins/ft_pwd.c
+++ sources/builtins/ft_pwd.c
@@ -6,25 +6,23 @@
 /*   By: lorobert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 09:33:27 by lorobert          #+#    #+#             */
-/*   Updated: 2023/02/11 09:42:54 by lorobert         ###   ########.fr       */
+/*   Updated: 2023/02/11 18:25:48 by lorobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <limits.h>
 #include "libft.h"
 
 int	ft_pwd(char **env, int fd)
 {
-	int	i;
+	char	cwd[PATH_MAX];
 
-	i = 0;
-	while (env[i])
+	(void)env;
+	if (getcwd(cwd, PATH_MAX))
 	{
-		if (ft_strncmp(env[i], "PWD=", 4) == 0)
-		{
-			ft_putstr_fd(&env[i][4], fd);
-			return (0);
-		}
-		i++;
+		ft_putstr_fd(cwd, fd);
+		return (0);
 	}
 	return (1);
 }
