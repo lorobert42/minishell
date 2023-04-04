68da1baa90efe16a8d1d29405ce46a3de33500ff
Arnaud Favre <afavre@student.42lausanne.ch>
2023-04-04 10:13:05 +0200

    norm pipe ok

--- sources/executer/executer.c
+++ sources/executer/executer.c
@@ -21,7 +21,8 @@ char	*find_path(t_data *data, int num)
 	i = 0;
 	while (path[i] != NULL)
 	{
-		if (access(get_path(path[i], data->table->commands[num].args[0]), F_OK | R_OK) == 0)
+		if (access(get_path(path[i], data->table->commands[num].args[0]), \
+		F_OK | R_OK) == 0)
 			return (get_path(path[i], data->table->commands[num].args[0]));
 		i++;
 	}
@@ -29,33 +30,42 @@ char	*find_path(t_data *data, int num)
 	return (NULL);
 }
 
-int	execute(t_data *data)
+void	children(t_data *data, int *prev_read, int i)
 {
-	int		prev_read = 0;
 	char	*path;
+
+	close(data->fd[0]);
+	dup2(*prev_read, STDIN_FILENO);
+	if (i < data->table->n_commands - 1)
+		dup2(data->fd[1], STDOUT_FILENO);
+	path = find_path(data, i);
+	if (path != NULL)
+	{
+		execve(path, data->table->commands[i].args, data->env);
+	}
+	else
+	{
+		ft_printf("🤷 Hérishell: %s: a pas trouver ... 🤷\n", \
+					data->table->commands->args[0]);
+		exit(0);
+	}
+}
+
+void	execution_loop(t_data *data)
+{
+	int		prev_read;
+	pid_t	id;
 	int		i;
 
 	i = 0;
+	prev_read = 0;
 	while (i < data->table->n_commands)
 	{
 		pipe(data->fd);
-		pid_t id = fork();
+		id = fork();
 		if (id == 0)
 		{
-			close(data->fd[0]);
-			dup2(prev_read, STDIN_FILENO);
-			if (i < data->table->n_commands - 1)
-				dup2(data->fd[1], STDOUT_FILENO);
-			path = find_path(data, i);
-			if (path != NULL)
-			{
-				execve(path, data->table->commands[i].args, data->env);
-			}
-			else
-			{
-				ft_printf("🤷 Hérishell: %s: a pas trouver ... 🤷\n", data->table->commands->args[0]);
-				exit(0);
-			}
+			children(data, &prev_read, i);
 		}
 		else
 		{
@@ -65,7 +75,10 @@ int	execute(t_data *data)
 		}
 		i++;
 	}
-	return (0);
 }
 
-
+int	execute(t_data *data)
+{
+	execution_loop(data);
+	return (0);
+}
