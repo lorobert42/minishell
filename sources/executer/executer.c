771aedd07fb9e6892bb12abfcd742e16c8cd99ba
lorobert <lorobert@student.42lausanne.ch>
2023-04-21 14:55:19 +0200

    File redirection wip

--- sources/executer/executer.c
+++ sources/executer/executer.c
@@ -6,7 +6,7 @@
 /*   By: lorobert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 12:04:43 by afavre            #+#    #+#             */
-/*   Updated: 2023/04/21 13:31:58 by lorobert         ###   ########.fr       */
+/*   Updated: 2023/04/21 14:50:42 by lorobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,16 +39,6 @@ void	children(t_data *data, int i)
 {
 	char	*path;
 
-	/* if (redirection_in(data, i))
-	{
-		close_redirections(data, i);
-		exit(1);
-	}
-	if (redirection_out(data, i, 0))
-	{
-		close_redirections(data, i);
-		exit(1);
-	} */
 	if (is_builtins(data->table->commands[i].args))
 		exec_builtins(data, data->table->commands[i].args);
 	else
@@ -62,7 +52,6 @@ void	children(t_data *data, int i)
 			ft_printf("🤷 Hérishell: %s: a pas trouver ... 🤷\n", \
 				data->table->commands[i].args[0]);
 	}
-	// close_redirections(data, i);
 	exit(0);
 }
 
@@ -80,42 +69,34 @@ void	execution_loop(t_data *data)
 		g_glob.nb_children += 1;
 		if (pid == 0)
 		{
-			if (i > 0)
-			{
-				dup2(data->table->commands[i - 1].fd[0], STDIN_FILENO);
-				if (close(data->table->commands[i - 1].fd[0]) == -1)
-					print_error(NULL, "close1");
-				if (close(data->table->commands[i - 1].fd[1]) == -1)
-					print_error(NULL, "close2");
-			}
-			if (i < data->table->n_commands - 1)
-			{
-				if (close(data->table->commands[i].fd[0]) == -1)
-					print_error(NULL, "close3");
-				dup2(data->table->commands[i].fd[1], STDOUT_FILENO);
-				if (close(data->table->commands[i].fd[1]) == -1)
-					print_error(NULL, "close4");
-			}
+			redirection_in(data, i);
+			redirection_out(data, i, 0);
 			children(data, i);
+			close_redirections(data, i);
 		}
 		else
 		{
 			if (i > 0)
 			{
 				if (close(data->table->commands[i - 1].fd[0]) == -1)
-					print_error(NULL, "close5");
+					print_error(NULL, ft_strjoin(data->table->commands[i].args[0], " close5"));
 				if (close(data->table->commands[i - 1].fd[1]) == -1)
-					print_error(NULL, "close6");
+					print_error(NULL, ft_strjoin(data->table->commands[i].args[0], " close6"));
+			}
+			else
+			{
+				if (close(data->table->commands[i].fd[1]) == -1)
+					print_error(NULL, ft_strjoin(data->table->commands[i].args[0], " close7"));
 			}
 		}
 		i++;
 	}
-	if (i > 0)
+	if (data->table->n_commands > 1)
 	{
 		if (close(data->table->commands[i - 1].fd[0]) == -1)
-			print_error(NULL, "close7");
+			print_error(NULL, ft_strjoin(data->table->commands[i - 1].args[0], " close8"));
 		if (close(data->table->commands[i - 1].fd[1]) == -1)
-			print_error(NULL, "close8");
+			print_error(NULL, ft_strjoin(data->table->commands[i - 1].args[0], " close9"));
 	}
 	while (g_glob.nb_children > 0)
 	{
@@ -132,6 +113,8 @@ void	execution_loop(t_data *data)
 
 int	execute(t_data *data)
 {
+	data->saved_io[0] = dup(STDIN_FILENO);
+	data->saved_io[1] = dup(STDOUT_FILENO);
 	set_heredoc(data);
 	if (data->table->n_commands == 1)
 	{
@@ -153,5 +136,7 @@ int	execute(t_data *data)
 		}
 	}
 	execution_loop(data);
+	dup2(data->saved_io[0], STDIN_FILENO);
+	dup2(data->saved_io[1], STDOUT_FILENO);
 	return (0);
 }
--- sources/executer/redirections.c
+++ sources/executer/redirections.c
@@ -6,7 +6,7 @@
 /*   By: lorobert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 10:15:13 by lorobert          #+#    #+#             */
-/*   Updated: 2023/04/21 12:51:19 by lorobert         ###   ########.fr       */
+/*   Updated: 2023/04/21 14:45:23 by lorobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,44 @@ int	redirection_in(t_data *data, int i)
 {
 	t_file	*current;
 	t_file	*prev;
+	int		fd;
 
 	current = data->table->commands[i].infiles;
-	if (!current && i > 0)
-	{
-		dup2(data->table->commands[i - 1].fd[0], STDIN_FILENO);
-		close(data->table->commands[i - 1].fd[0]);
-		close(data->table->commands[i - 1].fd[1]);
-	}
-	if (!current && i < data->table->n_commands - 1)
-	{
-		close(data->table->commands[i].fd[0]);
-		dup2(data->table->commands[i].fd[1], STDOUT_FILENO);
-		close(data->table->commands[i].fd[1]);
-	}
 	prev = NULL;
 	while (current)
 	{
 		if (prev)
 		{
-			close(data->table->commands[i].fd[0]);
+			close(fd);
 			if (prev->append)
 				unlink(prev->name);
 		}
-		data->table->commands[i].fd[0] = open(current->name, \
-			O_RDONLY);
-		if (data->table->commands[i].fd[0] == -1)
+		fd = open(current->name, O_RDONLY);
+		if (fd == -1)
 		{
 			print_error(NULL, current->name);
 			return (1);
 		}
-		dup2(data->table->commands[i].fd[0], STDIN_FILENO);
 		prev = current;
 		current = current->next;
+	}
+	if (i > 0 && data->table->commands[i].infiles)
+	{
+		if (close(data->table->commands[i - 1].fd[0]) == -1)
+			print_error(NULL, ft_strjoin(data->table->commands[i].args[0], " close0"));
+		data->table->commands[i - 1].fd[0] = fd;
+	}
+	else if (i == 0 && data->table->commands[i].infiles)
+	{
+		dup2(data->table->commands[i].fd[0], STDIN_FILENO);
+	}
+	if (i > 0)
+	{
+		dup2(data->table->commands[i - 1].fd[0], STDIN_FILENO);
+		if (close(data->table->commands[i - 1].fd[0]) == -1)
+			print_error(NULL, ft_strjoin(data->table->commands[i].args[0], " close1"));
+		if (close(data->table->commands[i - 1].fd[1]) == -1)
+			print_error(NULL, ft_strjoin(data->table->commands[i].args[0], " close2"));
 	}
 	return (0);
 }
@@ -57,38 +62,48 @@ int	redirection_out(t_data *data, int i, int solo)
 {
 	t_file	*current;
 	t_file	*prev;
+	int		fd;
 
+	(void)solo;
 	current = data->table->commands[i].outfiles;
-	if (!current && !solo && i < data->table->n_commands - 1)
-		dup2(data->table->commands[i].fd[1], STDOUT_FILENO);
 	prev = NULL;
 	while (current)
 	{
 		if (prev)
-			close(data->table->commands[i].fd[1]);
+			close(fd);
 		if (current->append)
 		{
-			data->table->commands[i].fd[1] = open(current->name, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-			if (data->table->commands[i].fd[1] == -1)
+			fd = open(current->name, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+			if (fd == -1)
 			{
 				print_error(NULL, current->name);
 				return (1);
 			}
-			dup2(data->table->commands[i].fd[1], STDOUT_FILENO);
 		}
 		else
 		{
-			data->table->commands[i].fd[1] = open(current->name, \
-			O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-			if (data->table->commands[i].fd[1] == -1)
+			fd = open(current->name, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+			if (fd == -1)
 			{
 				print_error(NULL, current->name);
 				return (1);
 			}
-			dup2(data->table->commands[i].fd[1], STDOUT_FILENO);
 		}
 		prev = current;
 		current = current->next;
+	}
+	if (data->table->commands[i].outfiles)
+	{
+		close(data->table->commands[i].fd[1]);
+		data->table->commands[i].fd[1] = fd;
+	}
+	if (i < data->table->n_commands - 1 || data->table->commands[i].outfiles)
+	{
+		if (close(data->table->commands[i].fd[0]) == -1)
+			print_error(NULL, ft_strjoin(data->table->commands[i].args[0], " close3"));
+		dup2(data->table->commands[i].fd[1], STDOUT_FILENO);
+		if (close(data->table->commands[i].fd[1]) == -1)
+			print_error(NULL, ft_strjoin(data->table->commands[i].args[0], " close4"));
 	}
 	return (0);
 }
